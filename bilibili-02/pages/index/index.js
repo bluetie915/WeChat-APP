4e52d9f8b4ed96e1fc89dfb03a9dda54bcaf3326
bluetie915 <1402711057@qq.com>
2020-01-07 17:44:18 +0800

    主页面开始完成

--- bilibili-02/pages/index/index.js
+++ bilibili-02/pages/index/index.js
@@ -9,13 +9,14 @@ Page({
     //首页导航数据
     navList: [],
     //轮播图数据
-    swiperList: []
+    swiperList: [],
+    //视频列表数据
+    videosList: []
   },
   //点击首页导航按钮
   activeNav(e) {
-    console.log(123);
     this.setData({
-      currentIndexNav: target.dataset.index
+      currentIndexNav: e.target.dataset.index
     })
   },
 
@@ -27,9 +28,8 @@ Page({
     //利用小程序内置发送请求的方法
     wx.request({
       url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/navList",
-      success(res){
-        console.log(res);
-        if(res.data.code===0) {
+      success(res) {
+        if (res.data.code === 0) {
           that.setData({
             navList: res.data.data.navList
           })
@@ -41,10 +41,32 @@ Page({
     获取轮播图
   */
   getSwiperList() {
+    let that = this;
     wx.request({
       url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/swiperList",
-      success(res){
+      success(res) {
+        if (res.data.code === 0) {
+          that.setData({
+            swiperList: res.data.data.swiperList
+          })
+        }
+      }
+    })
+  },
+  /* 
+    获取视频列表
+  */
+  getVideosList() {
+    let that = this;
+    wx.request({
+      url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/videosList",
+      success(res) {
         console.log(res);
+        if (res.data.code === 0) {
+          that.setData({
+            videosList: res.data.data.videosList
+          })
+        }
       }
     })
   },
@@ -56,54 +78,56 @@ Page({
     this.getNavList();
     // 2 调用获取轮播图数据
     this.getSwiperList();
+    // 3 调用获取视频列表函数
+    this.getVideosList();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
